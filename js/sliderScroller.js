bf9f53b4b0ff1a697f4bfafa3cce9d01ec61f616
yashgulati <yashgulati.g1@gmail.com>
2017-09-08 12:23:24 +0530

     

--- js/sliderScroller.js
+++ js/sliderScroller.js
@@ -1,11 +1,14 @@
-var scrolled=0;
 
 $(document).ready(function() {
+  console.log($("body").scrollTop());
+
   setTimeout(function(){
     var top = jQuery('#about').offset().top;
-  	$("body").animate({
-      scrollTop:  top - 10
-    });
+    if ($("body").scrollTop() == 0) {
+      $("body").animate({
+        scrollTop:  top - 10
+      });
+    }
   }, 5000);
 });
 
